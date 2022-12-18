be9049e14f5ff51a238ba2bf2db237d90a627e42
Jan Wielemaker <J.Wielemaker@vu.nl>
2022-12-18 17:05:59 +0100

    PORT: More odbc type work around.  As we do not use long [long] int sizes or availability, just leave them undefined and let unixODBC do its magic.

--- config.h.cmake
+++ config.h.cmake
@@ -1,6 +1,5 @@
 #cmakedefine HAVE_GMTIME @HAVE_GMTIME@
 #cmakedefine HAVE_LOCALTIME @HAVE_LOCALTIME@
-#cmakedefine HAVE_LONG_LONG @HAVE_LONG_LONG@
 #cmakedefine HAVE_MALLOC_H @HAVE_MALLOC_H@
 #cmakedefine HAVE_MKTIME @HAVE_MKTIME@
 #cmakedefine HAVE_SQLLEN @HAVE_SQLLEN@
@@ -8,7 +7,6 @@
 #cmakedefine HAVE_SQL_H @HAVE_SQL_H@
 #cmakedefine HAVE_TIMEGM @HAVE_TIMEGM@
 #cmakedefine HAVE_TIME_H @HAVE_TIME_H@
-#cmakedefine SIZEOF_LONG @SIZEOF_LONG@
 #cmakedefine SIZEOF_SQLWCHAR @SIZEOF_SQLWCHAR@
 #cmakedefine SIZEOF_WCHAR_T @SIZEOF_WCHAR_T@
 #cmakedefine _REENTRANT @_REENTRANT@
